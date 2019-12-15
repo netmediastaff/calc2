3d62772767bd7f9f50f1a8fc86186620730fee27
oghomwen.aigbedion <oghomwen.aigbedion@ISW-GSTAD-OA.Interswitchng.com>
2019-12-16 00:04:05 +0100

    switched it up

---
+++ src/main/java/com/holidu/interview/assignment/controller/Response.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.holidu.interview.assignment.controller;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ *
+ * @author oghomwen.aigbedion
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+
+    Boolean error;
+    String message;
+    Double result;
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" + "error=" + error + ", message=" + message + ", result=" + result + '}';
+    }
+
+}
--- src/main/java/com/holidu/interview/assignment/controller/TreeController.java
+++ src/main/java/com/holidu/interview/assignment/controller/TreeController.java
@@ -6,6 +6,8 @@
 package com.holidu.interview.assignment.controller;
 
 import com.holidu.interview.assignment.service.TreeService;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,10 +24,17 @@ public class TreeController {
 
     @Autowired
     private TreeService treeService;
+//
+//    @RequestMapping(value = "/{x}/{y}/{radius}", method = RequestMethod.GET)
+//    public Map getTrees(@PathVariable("x") double x, @PathVariable("y") double y, @PathVariable("radius") double radius) {
+//        return treeService.getTrees(x, y, radius);
+//    }
 
-    @RequestMapping(value = "/{x}/{y}/{radius}", method = RequestMethod.GET)
-    public Map getTrees(@PathVariable("x") double x, @PathVariable("y") double y, @PathVariable("radius") double radius) {
-        return treeService.getTrees(x, y, radius);
+    @RequestMapping(value = "calculus", method = RequestMethod.GET)
+    public Response doCalculation(@PathVariable("query") String expression) throws UnsupportedEncodingException {
+        String decodedExpression = new String(Base64.getDecoder().decode(expression.getBytes()), "UTF-8");
+        System.out.println(decodedExpression);
+        return Evaluator.evaluate(decodedExpression);
     }
 
 }
